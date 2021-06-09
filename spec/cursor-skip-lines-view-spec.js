b1495259f7dc95cb9cf01924d8708e46d1882f19
taka1226 <taka2637napoleon815@icloud.com>
2021-06-10 01:03:54 +0900

    initial commit

---
+++ spec/cursor-skip-lines-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CursorSkipLinesView from '../lib/cursor-skip-lines-view';
+
+describe('CursorSkipLinesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
